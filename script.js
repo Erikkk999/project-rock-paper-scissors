27b0d93a1f40b77f1120824012f050d63a579a84
Erikkk999 <193602698+Erikkk999@users.noreply.github.com>
2025-04-18 14:27:43 +0200

    Write code for computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    
+    if (random === 0) {
+        return "rock";
+    } else if (random === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+
+
