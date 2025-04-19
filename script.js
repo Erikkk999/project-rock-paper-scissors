4cbcea309149b2f6d58abd2e081c0e432c1e459c
Erikkk999 <193602698+Erikkk999@users.noreply.github.com>
2025-04-19 22:26:43 +0200

    Write code for single round
    and fix a mistake.

--- script.js
+++ script.js
@@ -6,33 +6,38 @@ function getComputerChoice() {
         return "rock";
     } else if (random === 1) {
         return "paper";
-    } else {
-        return "scissors";
+    } else if (random === 2) {
+        return "scissors"
     }
 }
 
 
 function getHumanChoice() {
     let person = prompt("rock, paper or scissors?", "choose").toLowerCase();
+    return person
 
-    switch(person) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            return getComputerChoice();
-    }
  }
 
-let humanScore = 0
-let computerScore = 0
+    let humanScore = 0
+    let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-if (humanChoice === computerChoice) {
-   console.log("Tie");
-}
 
-
-
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You Lost! Paper beats Rock.");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You Win! Paper beats Rock.");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You Lost! Rock beats Scissors.");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You Win! Rock beats Scissors.");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You Lost! Scissors beats Paper.");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You Win! Scissors beats Paper.");
+    } else if (humanChoice === computerChoice) {
+        console.log("Tie.");
+    }
 }
 
 const humanSelection = getHumanChoice();
